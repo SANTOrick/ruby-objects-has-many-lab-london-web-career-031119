0e8590a4da30bef82b094510ec00c7b4611fabe2
Riccardo <riccardo_london@live.it>
2019-03-17 09:20:47 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,17 @@
+class Author
+attr_accessor :name
+
+  def initialize(name, post)
+    @posts = []
+    @name = name
+    @posts << post
+  end
+
+  def add_post (post_name)
+    post_name = Post.new
+    @posts << self
+    
+
+  end
+
+end
